3f52efbad1fe3827ac6ec515ef29695808df1ff1
UsmanGTA <usmangta@gmail.com>
2020-04-28 08:54:21 +0000

    fixed code for task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-for sodas in range(0, 101):
+for sodas in range(1, 101):
     if sodas % 15 == 0:
         print("FizzBuzz", end=' ')
-        continue
+    elif sodas == 100:
+        print("Buzz", end='')
     elif sodas % 3 == 0:
         print("Fizz", end=' ')
     elif sodas % 5 == 0:
         print("Buzz", end=' ')
-    print("{:d}".format(sodas), end=' ')
+    else:
+        print("{:d}".format(sodas), end=' ')
