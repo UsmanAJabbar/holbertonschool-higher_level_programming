c25deb9c961df874c3213d5c23a49995e8677e57
UsmanGTA <usmangta@gmail.com>
2020-04-28 18:04:46 +0000

    fixed with elif statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,18 @@
 import random
 
 number = random.randint(-10000, 10000)
-ld = number % 10
+if number < 0:
+    mnumber = number * -1
+else:
+    mnumber = number
+ld = mnumber % 10
 firsthalf = "Last digit of {:d} is {:d}"
 
 if ld > 5:
     condition = " and is greater than 5"
-if ld == 0:
+elif ld == 0:
     condition = " and is 0"
-if ld < 6:
+elif ld < 6:
     condition = " and is less than 6 and not 0"
 
 print(firsthalf.format(number, ld) + condition)
