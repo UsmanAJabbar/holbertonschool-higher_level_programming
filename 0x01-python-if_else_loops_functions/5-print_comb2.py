e6262db528400ab958f6d8fba82082f7f03c3e58
UsmanGTA <usmangta@gmail.com>
2020-04-28 18:10:34 +0000

    Adding trailing zeros for single digit numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 for numbers in range(0, 100):
-    print("{:1d}".format(numbers), end='')
+    print("{:02d}".format(numbers), end='')
     if numbers < 99:
-        print(", ", end='')
+        print(", ", end=' ')
+    else:
+        print("")
