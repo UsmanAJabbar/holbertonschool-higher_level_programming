53e09e5606de7048429bc507626360dd1618f330
UsmanGTA <usmangta@gmail.com>
2020-04-29 01:46:10 +0000

    defined function

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
-for sodas in range(1, 101):
-    if sodas % 15 == 0:
-        print("FizzBuzz", end=' ')
-    elif sodas == 100:
-        print("Buzz ")
-    elif sodas % 3 == 0:
-        print("Fizz", end=' ')
-    elif sodas % 5 == 0:
-        print("Buzz", end=' ')
-    else:
-        print("{:d}".format(sodas), end=' ')
+def fizzbuzz():
+    for sodas in range(1, 101):
+        if sodas % 15 == 0:
+            print("FizzBuzz", end=' ')
+        elif sodas == 100:
+            print("Buzz ")
+        elif sodas % 3 == 0:
+            print("Fizz", end=' ')
+        elif sodas % 5 == 0:
+            print("Buzz", end=' ')
+        else:
+            print("{:d}".format(sodas), end=' ')
