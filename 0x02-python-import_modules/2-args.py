d583a76b63864d15287319bf1e0abed8f44423d4
UsmanGTA <usmangta@gmail.com>
2020-04-30 19:05:42 +0000

    added initial code for task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+# IMPORT GUARD
+if __name__ == '__main__':
+    argc = (len(argv) - 1)  # -1 to exclude the program filename count
+
+    # argc check
+    if argc <= 0:
+        print("{:d} arguments.".format(argc))
+    if argc > 0:
+        print("{:d} arguments.".format(argc))
+        # Print all of the words ( + 1 to print exclude file name )
+        for wordindex in range(argc):
+            print("{:d}: {}".format((wordindex + 1), argv[wordindex + 1]))
