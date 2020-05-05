251a0a7d376fe2bfbb57c7d5fe21607039ac11d5
UsmanGTA <usmangta@gmail.com>
2020-05-05 17:51:20 +0000

    added code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,13 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for height in range(len(matrix)):
-        for width in range(len(matrix[height])):
-            print("{:d}".format(matrix[height][width]), end="")
-        print("")
+    if matrix:
+        for height in range(len(matrix)):
+
+            for width in range(len(matrix[height])):
+
+                print("{:d}".format(matrix[height][width]), end="")
+                if width < len(matrix[height]) - 1:
+                    print(" ", end='')
+
+            print("")
