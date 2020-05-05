1f3cf78d1a22505810afb9eba7d44768bb723989
UsmanGTA <usmangta@gmail.com>
2020-05-05 23:52:48 +0000

    added

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,8 @@
 
 
 def multiple_returns(sentence):
-    if sentence:
-        strlen = len(sentence)
-        if strlen == 0:
-            return strlen, None
-        firstchar = sentence[0]
-        return strlen, firstchar
+    strlen = len(sentence)
+    if strlen == 0:
+        return strlen, None
+    firstchar = sentence[0]
+    return strlen, firstchar
