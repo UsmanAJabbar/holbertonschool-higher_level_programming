9254987c59ddffeddb5f221ad3b749f5c77a097d
UsmanGTA <usmangta@gmail.com>
2020-05-05 17:19:28 +0000

    fixed

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+
 def replace_in_list(my_list, idx, element):
     # Check if that index exists
     if idx < 0:
         return(my_list)
 
-    elementcount = len(my_list)
+    elementcount = len(my_list) - 1
     if elementcount < idx:
         return(my_list)
 
