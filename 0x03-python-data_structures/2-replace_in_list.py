3b9a70649ab5f8b945e33daa748b0407606b9925
UsmanGTA <usmangta@gmail.com>
2020-05-05 14:10:18 +0000

    added initial code for task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    # Check if that index exists
+    if idx < 0:
+        return(my_list)
+
+    elementcount = len(my_list)
+    if elementcount < idx:
+        return(my_list)
+
+    # That index definitely exists, modify the data
+    else:
+        my_list[idx] = element
+        return(my_list)
