5f4fe90874cf7b6a646fcc7a6e1e736c41d8dee1
UsmanGTA <usmangta@gmail.com>
2020-05-07 17:13:34 +0000

    added fixed code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -10,5 +10,5 @@ def search_replace(my_list, search, replace):
     for indexes in range(len(my_list)):
         if new_list[indexes] == search:
             new_list[indexes] = replace
-
-    return new_list
+            return new_list
+    return None
