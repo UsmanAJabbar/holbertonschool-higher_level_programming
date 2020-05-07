fc4fb512152df2cebb36a2578ba89bda8754724e
UsmanGTA <usmangta@gmail.com>
2020-05-07 18:37:51 +0000

    added initial code

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    if not set_1 and set_2:
+        return None
+
+    final_list = []
+
+    for specific_elements in set_1 and set_2:
+        if specific_elements in set_1 and set_2:
+            final_list.append(specific_elements)
+    return final_list
