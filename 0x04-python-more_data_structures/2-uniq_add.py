c65021b004ffc776d31c1b702c3538324e1cc216
UsmanGTA <usmangta@gmail.com>
2020-05-07 18:09:58 +0000

    added intial code for task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    if not my_list:
+        return None
+    new_list = my_list[:]  # Creating a copy of the original list
+    fodups = set(new_list) # Removing Duplicates
+    sum = 0 # Initializing sum to 0 for the loop
+    
+    for elements in fodups:
+        sum = sum + elements
+
+    return sum
