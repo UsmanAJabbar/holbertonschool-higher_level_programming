4ae2c599e861da07e9b610814a5be3869e8eb735
UsmanGTA <usmangta@gmail.com>
2020-05-08 01:07:30 +0000

    added initial code

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+
+    copied = dict(a_dictionary)
+    delcount = 0  # Number of keys deleted
+    
+    # Loop through the dictionary, if value's
+    # found, then delete and then continue search
+    for keys, v in copied.items():
+        if v == value:
+            del a_dictionary[keys]
+            delcount += 1
+
+    if delcount == 0:
+        return copied
+    else:
+        return a_dictionary
