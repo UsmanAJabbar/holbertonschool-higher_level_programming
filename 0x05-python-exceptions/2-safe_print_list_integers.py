3598abba80bd5d72669a9a458440c225e9abfe39
UsmanGTA <usmangta@gmail.com>
2020-05-18 16:15:25 +0000

    added initial code for task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    dem_legit_values = 0
+    
+    for elements in range(x):
+        try:
+            print("{:d}".format(my_list[elements]), end='')
+            dem_legit_values += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return dem_legit_values
