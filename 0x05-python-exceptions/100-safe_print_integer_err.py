cd93c73be1e327a8815117d42cdd06056297f1ee
UsmanGTA <usmangta@gmail.com>
2020-05-18 19:00:36 +0000

    Fixed code

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,15 +4,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
-        err = "Exception: Unknown format code 'd' for object of type "
-        if isinstance(value, int):
-            objtype = "'int'"
-        elif isinstance(value, list):
-            objtype = "'list'"
-        elif isinstance(value, str):
-            objtype = "'str'"
-        elif isinstance(value, float):
-            objtype = "'float'"
-        stderr.write(err + objtype + '\n')
+    except (ValueError, TypeError) as err:
+        errormessage = "Exception: " + str(err) + '\n'
+        stderr.write(errormessage)
         return False
