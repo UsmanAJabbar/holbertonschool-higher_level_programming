6b859b44738715ebcfb5ebbfc8764aa07508be28
UsmanGTA <usmangta@gmail.com>
2020-05-18 17:11:36 +0000

    Updated with PEP8 style requirements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     elementcount = 0    # Counter to count the number of elements printed
 
@@ -8,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[elements], end='')
             elementcount += 1
 
-        except IndexError:  # If x is greater than the number of elements in my_list, break
+        except IndexError:
                 break
     print()
-    
-    return elementcount # Return the number of elements successfully parsed
+
+    return elementcount  # Return the number of elements successfully parsed
