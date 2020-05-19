62be840c488ba17db3cf857358bc85d2423e9b1b
UsmanGTA <usmangta@gmail.com>
2020-05-19 21:07:21 +0000

    added PEP8/Docstring Fixes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""Class Square"""
+
+
 class Square:
+    """Square Class with attributes"""
     __size = None
 
-    def __init__(self, __size = None):
+    def __init__(self, __size=None):
         """
         Method that initializes the class and checks
         for good input
         Args:
             self: refers back to the class
             size: default = 0, else defined input
-        """    
+        """
         self.__size = __size
