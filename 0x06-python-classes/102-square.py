49e6fcc7662b1c35ff0f8c9fa65e232c37c5e6dd
Usman Abdul Jabbar <46794411+UsmanGTA@users.noreply.github.com>
2020-05-19 21:26:58 -0500

    Update 102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Class Square"""
+
+
 class Square:
+    """Square Initialization"""
     def __init__(self, size=0):
         """
         Method that initializes the class and checks
