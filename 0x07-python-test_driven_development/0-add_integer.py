fab88ff8687597e3b52da3f7036bf8929a7b4615
UsmanGTA <usmangta@gmail.com>
2020-05-21 20:16:14 +0000

    added initial commits

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,15 +11,15 @@ def add_integer(a, b=98):
         a = Requires user input
         b = If not defined, defaults to 98
     """
-
-    if not a:
+    # Check if we have good inputs, a must exist to continue
+    if a is None:
         raise TypeError("add_integer() missing 1 required positional argument: 'a'")
 
-    if a:
-        if type(a) is not int or type(a) is not float:
+    # If a exists, check data types
+    if type(a) is not int and type(a) is not float:
             raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+            raise TypeError("b must be an integer")
 
-    if type(b) is not int or type(b) is not float:
-        raise TypeError("b must be an integer")
-
+    # If they're floats/ints, cast and return ints
     return int(a) + int(b)
