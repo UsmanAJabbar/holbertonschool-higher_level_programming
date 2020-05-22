eb4c62d70762c9399ee2afd0f5ce8d7889f837f1
UsmanGTA <usmangta@gmail.com>
2020-05-22 17:30:48 +0000

    added initial code

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""DEFINING A RECTANGLE"""
+
+
+class Rectangle:
+    """
+    ----------------
+    CLASS: RECTANGLE
+    ----------------
+    Description:
+        Defines a new class called rectangle
+    Exceptions/Notes:
+        Does nothing, hence, pass.
+    Args:
+        Nil
+    """
+    pass
