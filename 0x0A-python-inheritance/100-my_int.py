f692a638fc4d9701afa7c00c9aae0d660630c512
usmangta <usmangta@gmail.com>
2020-06-02 19:47:18 +0000

    added initial commit for advanced task 1

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""THE REBEL"""
+
+
+class MyInt(int):
+    """
+    ------------
+    CLASS: MyInt
+    ------------
+    """
+
+    def __eq__(self, other):
+    """The not equal rebel"""
+        return not int(self)
+
+    def __ne__(self, other):
+    """The equal rebel"""
+        return int(self) == int(self)
