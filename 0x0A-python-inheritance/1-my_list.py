68b13d12b95a2720da09794b2813431015afbda5
usmangta <usmangta@gmail.com>
2020-06-02 06:40:53 +0000

    added initial code for task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A Random Class, Move On"""
+
+class MyList(list):
+    """"
+    ----------
+    METHOD: LITERALLY DOES NOTHING
+    ----------
+    Description:
+        To be argued.
+    Args:
+        @list: list from main
+    """""
+
+    def print_sorted(self):
+        """
+        --------------------
+        METHOD: PRINT_SORTED
+        --------------------
+        Description:
+            Prints out a list sorted in ascending order.
+        Args:
+            @self: inherits the input from self
+        """
+        print(sorted(self))
