23c05a564abfc0f331823eed573d924a5825d6ad
usmangta <usmangta@gmail.com>
2020-06-02 17:26:17 +0000

    added initial code for task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """
+    ---------------------
+    METHOD: IS SAME CLASS
+    ---------------------
+    Description:
+        Verifies whether two objects given are of the
+        same class.
+    Args:
+        @obj: object
+        @a_class: class
+    """
+    return type(obj) == a_class
