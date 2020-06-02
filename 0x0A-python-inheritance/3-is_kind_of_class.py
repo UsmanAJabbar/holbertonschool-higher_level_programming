5713795ac66bdd2cb4f6c82f5d8d0a083431f657
usmangta <usmangta@gmail.com>
2020-06-02 06:59:42 +0000

    added initial code for task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def is_kind_of_class(obj, a_class):
+    """
+    ---------------------
+    METHOD: IS SAME CLASS
+    ---------------------
+    Description:
+        Verifies whether two objects given are of the
+        same class.
+    Args:
+        @obj: object
+        @a_class: class
+    """
+    return True if isinstance(obj, a_class) else False
