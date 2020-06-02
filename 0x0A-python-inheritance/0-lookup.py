4a0ed34f6e87ade81c33b3f6f8afb927b665a355
usmangta <usmangta@gmail.com>
2020-06-02 06:38:03 +0000

    added initial commits

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    --------------
+    METHOD: LOOKUP
+    --------------
+    Description:
+        Function that looks up all the attributes and
+        methods associated with an object.
+    Args:
+        @obj: object imported from main
+    --------------
+    """
+    return dir(obj)
