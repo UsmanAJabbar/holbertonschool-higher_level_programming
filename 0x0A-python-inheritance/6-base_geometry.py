508fa6895c24b50b4ef3af4a39dff25fad3036d1
usmangta <usmangta@gmail.com>
2020-06-02 07:23:36 +0000

    added initial code for task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""BASE GEOMETRY CLASS"""
+
+
+class BaseGeometry:
+    """
+    --------------------
+    CLASS: BASE GEOMETRY
+    --------------------
+    """
+    pass
+
+    def area(self):
+        """
+        ------------
+        METHOD: AREA
+        ------------
+        Description:
+            A function that raises a TypeError
+        Args:
+            @self: refers back to the class
+        """
+        raise Exception("area() is not implemented")
