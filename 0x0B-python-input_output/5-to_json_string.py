73e7caa7882e8ed36d45b9dee869601780abd2ad
usmangta <usmangta@gmail.com>
2020-06-03 18:10:59 +0000

    added initial commit for task 5

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A RANDOM DOCSTRING GUYS, MOVE ON"""
+
+
+def to_json_string(my_obj):
+    """
+    ----------------------
+    METHOD: TO JSON STRING
+    ----------------------
+    Description:
+        Serializes given input into JSON
+    Args:
+        @my_obj: object to be serialized
+    """
+    import json
+    return json.dumps(my_obj)
