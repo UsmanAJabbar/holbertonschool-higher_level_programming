b23ea38dcf93c0d1633d5e571159c541e77f2ff6
usmangta <usmangta@gmail.com>
2020-06-03 21:15:05 +0000

    added docstring

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+----------------
+SCRIPT: ADD ITEM
+----------------
+Description:
+    Takes input from stdin and appends it
+    onto a JSON list
+"""
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
