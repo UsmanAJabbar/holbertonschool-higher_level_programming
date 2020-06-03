b855f386cbaddf3918342f89c8d7f70499d21fc7
usmangta <usmangta@gmail.com>
2020-06-03 22:42:35 +0000

    added initial commit for task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -26,7 +26,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
+    def to_json(self):
         """"
         ---------------
         METHOD: TO JSON
