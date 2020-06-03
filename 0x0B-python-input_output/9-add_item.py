3299d2b8c6efc1c14a17758b33c8e4e24fc18826
usmangta <usmangta@gmail.com>
2020-06-03 22:30:05 +0000

    made pep8 happy

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 """
 ----------------
 SCRIPT: ADD ITEM
@@ -7,17 +10,13 @@ Description:
     Takes input from stdin and appends it
     onto a JSON list
 """
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-from sys import argv
 try:
     my_list = load_from_json_file("add_item.json")
     my_list = list(my_list)
 except Exception:
     my_list = []
-    save_to_json_file(my_list,"add_item.json")
-    
+    save_to_json_file(my_list, "add_item.json")
+
 for elements in range(1, len(argv)):
     my_list.append(argv[elements])
 
