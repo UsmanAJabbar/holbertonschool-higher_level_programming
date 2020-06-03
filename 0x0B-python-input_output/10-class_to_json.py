12ecbdda38b63870536dbba128301bf0ee93d279
usmangta <usmangta@gmail.com>
2020-06-03 21:34:07 +0000

    added initial commit for task 10

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""YET ANOTHER WILD DOCSTRING APPEARS"""
+
+
+def class_to_json(obj):
+    """
+    ---------------------
+    METHOD: CLASS TO JSON
+    ---------------------
+    Descriptions:
+        Returns dictionary method of a given class
+    Args:
+        @obj: a class imported from main
+    """
+    return obj.__dict__
