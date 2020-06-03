a59d745fb770754ad3c7406133f487f9918cdb1f
usmangta <usmangta@gmail.com>
2020-06-03 21:37:16 +0000

    added initial commit for task 10

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -12,4 +12,6 @@ def class_to_json(obj):
     Args:
         @obj: a class imported from main
     """
+    # Returns the dictionary section of the class object
+    # since the __dict__ sections always hold the attributes
     return obj.__dict__
