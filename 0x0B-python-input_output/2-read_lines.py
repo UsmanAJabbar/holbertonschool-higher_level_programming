ad0f199f2402d73f06437f815f079eaf6d9063b0
usmangta <usmangta@gmail.com>
2020-06-03 16:51:29 +0000

    added initial commits for task 2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A RANDOM DOCSTRING FOLKS"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+    ------------------
+    METHOD: READ LINES
+    ------------------
+    Description:
+        Method that reads a certain number of
+        lines from a file and prints it out
+        to standard output.
+    Args:
+        @filename: file name
+        @nb_lines: number of lines to read
+    """
+    with open(filename, mode="r", encoding="utf-8") as buffer:
+        # If this method is called without nb_lines defined
+        # print the whole buffer
+        if nb_lines <= 0:
+          print(buffer.read(), end="")
+          return 0
+
+        # Else, print up until nblines
+        linecount = 0
+        for strings in buffer:
+            print(strings, end="")
+            linecount += 1
+            if linecount > nb_lines:
+                return 0
