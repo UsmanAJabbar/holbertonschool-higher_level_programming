4cb416480db3b3eea409cf0865bb2ebf90aa1261
usmangta <usmangta@gmail.com>
2020-07-13 09:31:43 +0000

    added initial commit for task 17

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,13 @@
+-- Convert the database hbtn_0c_0, first_table,
+-- and name to UTF8 format
+ALTER DATABASE hbtn_0c_0
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table
+	CONVERT TO CHARACTER
+	SET utf8mb4 COLLATE
+	utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table
+	MODIFY name VARCHAR(255)
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
