f99e82096a83a38f0a580232b9ce83d571a3a26e
UsmanGTA <usmangta@gmail.com>
2020-07-14 21:09:28 +0000

    added DESC

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ SELECT tv_genres.name AS genre, count(*) as number_of_shows
 	FROM tv_show_genres LEFT JOIN tv_genres
 	ON tv_show_genres.genre_id = tv_genres.id
 	GROUP BY tv_genres.name
-	ORDER BY number_of_shows;
+	ORDER BY number_of_shows DESC;
