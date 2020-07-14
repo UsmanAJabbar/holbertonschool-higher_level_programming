124d066d0ee3f981670b011886b87714939f50e9
UsmanGTA <usmangta@gmail.com>
2020-07-14 21:10:31 +0000

    added DESC

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Counts the number of shows associated
 -- with each and every genre.
-SELECT tv_genres.name AS genre, count(*) as number_of_shows
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows
 	FROM tv_show_genres LEFT JOIN tv_genres
 	ON tv_show_genres.genre_id = tv_genres.id
 	GROUP BY tv_genres.name
