5f6d8d4da0f9b7ab58e39fc269e0f681b5b50111
UsmanGTA <usmangta@gmail.com>
2020-07-14 15:19:58 +0000

    added working code for task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,5 @@
 -- California by running a subquery
 SELECT `cities`.id, cities.name
 	FROM `states`, `cities`
-	WHERE cities.state_id = states.id;
+	WHERE cities.state_id = states.id
+	AND states.name = 'California';
