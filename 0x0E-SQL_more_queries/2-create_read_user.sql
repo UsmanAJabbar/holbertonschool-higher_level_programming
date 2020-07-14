228e19298f6a6198a8a5c42e97de3430d76153c8
usmangta <usmangta@gmail.com>
2020-07-14 08:05:06 +0000

    added commits

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Creates a database by the name hbtn_0d_2
+-- and user user_0d_2 identified by user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`;
+FLUSH PRIVILEGES;
