0af47ad10627da7441db73632d6f92143a9dd379
UsmanGTA <usmangta@gmail.com>
2020-08-08 18:18:40 +0000

    added fixes

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Takes in a search query and returns the id if it exists"""
 
 
@@ -10,11 +10,11 @@ if __name__ == "__main__":
 
     # Connect to the database
     user, passwd, database, search_term = argv[1], argv[2], argv[3], argv[4]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.\
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(user, passwd, database), pool_pre_ping=True)
 
     # Bind the database details and create a session
-    Session = sessionmaker(bind = engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     # Fetch results by generating a query
