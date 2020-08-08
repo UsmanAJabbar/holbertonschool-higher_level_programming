bbc07e0c8b30cd52cdb2e2aaac8aedbd0134f513
UsmanGTA <usmangta@gmail.com>
2020-08-08 22:41:24 +0000

    added initial commit for task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Deletes All States that have an 'a' in them"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+
+    # Connect to the database
+    user, passwd, database = argv[1], argv[2], argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, database), pool_pre_ping=True)
+
+    # Bind the database details and create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Pulls out all the states with an 'a' in them
+    result = session.query(State).filter(State.name.like('%a%')).all
+
+    # Loop through and delete all of them individually
+    for states in result:
+        session.delete(states)
+
+    # Commit changes
+    session.commit()
