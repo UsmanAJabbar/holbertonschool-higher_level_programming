6705f898ff259fc0939c69a162f9b5c39156fbc3
UsmanGTA <usmangta@gmail.com>
2020-08-08 17:55:25 +0000

    changed FROM to INNER JOIN

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
 
     # Use the cursor to execute the command
     lookup.execute("SELECT cities.id, cities.name, states.name\
-                    FROM cities, states\
-                    WHERE cities.state_id=states.id\
+                    FROM cities INNER JOIN states\
+                    ON cities.state_id=states.id\
                     ORDER BY states.id ASC;")
 
     # Fetch all the data
