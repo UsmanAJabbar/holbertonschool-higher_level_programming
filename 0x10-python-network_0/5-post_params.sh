af8e0ec690d018b93673be2d8248ae2735f61182
UsmanGTA <usmangta@gmail.com>
2020-08-18 17:09:49 +0000

    added fixes for task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a POST request at that specific URL with email and parameters pre-defined
-curl -s "$1" -X POST -F subject='I will always be here for PLD' -F name=hr@holbertonschool.com
+curl -s "$1" -X POST -F subject='I will always be here for PLD' -F name="hr@holbertonschool.com"
