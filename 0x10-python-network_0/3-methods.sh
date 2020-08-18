61a918f7db567a7d41ad9d6f8a73576133175ae9
UsmanGTA <usmangta@gmail.com>
2020-08-18 16:19:48 +0000

    added initial commit for task 4

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a request to a valid URL in $1 and extracts the content-length
-curl -sI "$1" | grep Allow | awk '{ print $2 }'
+curl -sI "$1" | grep Allow | cut -d ':' -f 2 | sed 's/ //'
