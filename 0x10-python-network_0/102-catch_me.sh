70d074a897b335870819a9d5a10561cfcf14b470
UsmanGTA <usmangta@gmail.com>
2020-08-18 20:34:20 +0000

    removed extra newline

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Finds a pesky wabbit dug under the server with
-curl -sL 0.0.0.0:5000/catch_me -X PUT -F 'user_id=98' -H 'Origin: HolbertonSchool'; echo ""
+curl -sL 0.0.0.0:5000/catch_me -X PUT -F 'user_id=98' -H 'Origin: HolbertonSchool'; echo "" | tr -d '\n'
