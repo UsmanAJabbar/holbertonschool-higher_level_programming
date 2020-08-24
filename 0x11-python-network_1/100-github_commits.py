3bd26a1f5bfc35dc2edce9140277e5fde2840d54
UsmanGTA <usmangta@gmail.com>
2020-08-24 20:53:22 +0000

    fixed stupid output

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
 
     url, owner, repo = "https://api.github.com/repos/", argv[1], argv[2]
 
-    response1 = requests.get(url + owner + '/' + repo + '/commits')
-    dictionary = response1.json()
+    connect = requests.get(url + owner + '/' + repo + '/commits')
+    response = connect.json()
 
     count = 0
-    for elements in dictionary:
-        print(elements['sha'], elements['commit']['author']['name'])
+    for dictionaries in response:
+        print(dictionaries['sha'] +  ': ' +  dictionaries['commit']['author']['name'])
         count += 1
         if count == 10:
             break
