1f7fd401577db3862d4faad079c77e399640c4a4
UsmanGTA <usmangta@gmail.com>
2020-08-24 21:41:28 +0000

    shortened count

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,11 +9,7 @@ if __name__ == "__main__":
     url, owner, repo = "https://api.github.com/repos/", argv[2], argv[1]
 
     connect = requests.get(url + owner + '/' + repo + '/commits')
-    response = connect.json()
+    response = connect.json()[:10]
 
-    count = 0
     for dicts in response:
         print(dicts['sha'] + ': ' + dicts['commit']['author']['name'])
-        count += 1
-        if count == 10:
-            break
