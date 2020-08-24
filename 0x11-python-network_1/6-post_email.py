350c514e3a2b837b75d079cb8eaec912e706e0ad
UsmanGTA <usmangta@gmail.com>
2020-08-24 19:00:43 +0000

    fixed url variable

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     parameters = {'email': email}
 
     # Process data
-    response = requests.post('url', parameters)
+    response = requests.post(url, data=parameters)
     print(response.text)
