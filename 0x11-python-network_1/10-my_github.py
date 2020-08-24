9e2644a8fed62c8d4abd755db630991a8b738dd0
UsmanGTA <usmangta@gmail.com>
2020-08-24 20:03:06 +0000

    added initial commit for task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""MY GITHUB, VERY KEWL!!!"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    url, user, token = 'https://api.github.com/user', argv[1], argv[2]
+    out = requests.get(url, auth=(user, token))
+    id = (out.json()['id'])
+    print(id)
