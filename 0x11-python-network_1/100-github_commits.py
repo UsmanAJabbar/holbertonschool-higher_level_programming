516b7b156192255ccfc80a9cf999bd3823d66785
UsmanGTA <usmangta@gmail.com>
2020-08-24 20:35:40 +0000

    added initial commit for task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A WILD COMMENT APPEARS"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    url, owner, repo = "https://api.github.com/repos/", argv[1], argv[2]
+
+    response1 = requests.get(url + owner + '/' + repo + '/commits')
+    dictionary = response1.json()
+
+    count = 0
+    for elements in dictionary:
+        print(elements['sha'])
+        count += 1
+        if count == 10:
+            break
