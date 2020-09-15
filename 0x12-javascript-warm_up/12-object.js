5cda5a3efcc992533492aebeef7ce238e645b47e
UsmanGTA <usmangta@gmail.com>
2020-09-15 15:44:57 +0000

    added initial commit for task 12

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Updates an attribute of a specific object
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
