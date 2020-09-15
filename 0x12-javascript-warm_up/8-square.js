2582fff00679f948b6582df1dc9f7fc7fa47eb87
UsmanGTA <usmangta@gmail.com>
2020-09-15 14:00:31 +0000

    added initial commit for task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Prints a square based off of the size defined by input
+!isNaN(process.argv[2]) ? square(process.argv[2]): console.log('Missing size')
+
+/**
+ * square - prints out a square
+ * @sq_size: size of the square
+ * Return: None
+ */
+function square(sq_size) {
+  let buffer = '';
+  let size = parseInt(sq_size);
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      buffer += 'X';
+    }
+    i !== size - 1 ? buffer += '\n' : {};
+  }
+  console.log(buffer);
+}
