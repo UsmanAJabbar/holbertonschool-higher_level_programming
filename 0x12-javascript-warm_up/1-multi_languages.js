836172575dc00f91aad625cf47e353ee1541b40b
UsmanGTA <usmangta@gmail.com>
2020-09-15 12:18:31 +0000

    added initial commit for task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prints out a message to the log
+const c = 'C is fun';
+const python = 'Python is cool';
+const java = 'Javascript is amazing';
+console.log(c + '\n' + python + '\n' + java);
