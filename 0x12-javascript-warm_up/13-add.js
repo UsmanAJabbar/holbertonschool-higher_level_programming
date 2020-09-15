da77cd21ea8c0cca2948cc17e966790c7189e72f
UsmanGTA <usmangta@gmail.com>
2020-09-15 15:51:59 +0000

    added initial commit for task 13

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Adds two numbers
+/**
+ * add - adds two numbers
+ * num1 - first number
+ * num2 - second number
+ * Return: None
+ */
+exports.add = function (num1, num2) {
+  return parseInt(num1) + parseInt(num2);
+}
