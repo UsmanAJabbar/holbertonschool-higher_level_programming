ca0b51a71130be44d6e990819f05762b09e188cb
UsmanGTA <usmangta@gmail.com>
2020-09-15 13:31:22 +0000

    added initial commit for task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prints an array with one lie
+let lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for(let index = 0; index < lang.length; index++) {
+  console.log(lang[index]);
+}
