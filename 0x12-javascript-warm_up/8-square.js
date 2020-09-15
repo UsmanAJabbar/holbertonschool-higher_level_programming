39b8fe4146c67ad6c1941b4144b75299b98ca2f5
UsmanGTA <usmangta@gmail.com>
2020-09-15 14:03:22 +0000

    added semistandard fixes

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 // Prints a square based off of the size defined by input
-!isNaN(process.argv[2]) ? square(process.argv[2]): console.log('Missing size')
+!isNaN(process.argv[2]) ? square(process.argv[2]) : console.log('Missing size');
 
 /**
  * square - prints out a square
  * @sq_size: size of the square
  * Return: None
  */
-function square(sq_size) {
+function square (sq_size) {
   let buffer = '';
-  let size = parseInt(sq_size);
+  const size = parseInt(sq_size);
 
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
