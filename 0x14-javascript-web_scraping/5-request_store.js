55625e4d473cdf84cfa40d70a9bdfeb9bde120f3
UsmanGTA <usmangta@gmail.com>
2020-09-22 20:13:48 +0000

    added cleaner code

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
-// Fetches the content from a URL specified in
-// the terminal saves the content to a file defined
-// by the terminal
-const fs = require('fs')
-const req = require('request')
-const end = process.argv[2]
-const filename = process.argv[3]
+// Fetches the content from a URL specified in the
+// terminal saves the content to a file defined by the terminal
+const fs = require('fs');
+const req = require('request');
+const url = process.argv[2];
+const filename = process.argv[3];
 
-if (end && filename) {
-  req(end, function (err, res, body) {
-    fs.writeFile(filename, body, 'utf8', function (err, data) {
-      data = (err) ? console.log(err) : {};
-    });
-  }
-)}
+if (url && filename) {
+  req(url, function (err, res, body) {
+    (err) ? console.log(err) : fs.writeFileSync(filename, body);
+  });
+}
