d9fc09ecf7e9b0b3b1545ee172d6030211be05cf
UsmanGTA <usmangta@gmail.com>
2020-09-22 21:37:55 +0000

    added initial commit for task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// Counts the number of tasks each user has completed
+const req = require('request');
+const url = process.argv[2];
+
+if (url !== undefined) {
+  req(url, function (err, response, jsonstr) {
+    const json = JSON.parse(jsonstr);
+    const tasks = {};
+
+    for (var objects of json) {
+
+      // If the user ID doesn't exist in the dictionary, add it
+      if (tasks[objects['userId']] === undefined) {
+        tasks[objects['userId']] = 0;
+      }
+
+      // Else, check if the task was completed
+      if (objects['completed']) {
+        tasks[objects['userId']] += 1;
+      }
+
+    }
+    console.log(tasks)
+  });
+}
