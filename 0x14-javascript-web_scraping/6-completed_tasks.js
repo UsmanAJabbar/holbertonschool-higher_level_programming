673ac3187327d8ca3c192a5193be1422d3526ba2
UsmanGTA <usmangta@gmail.com>
2020-09-22 21:48:00 +0000

    attempt 2 at making the checker happy

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,15 +4,17 @@ const req = require('request');
 const url = process.argv[2];
 
 if (url !== undefined) {
-  req(url, function (err, response, jsonstr) {
+  req(url, (err, response, jsonstr) => {
     const json = JSON.parse(jsonstr);
     const tasks = {};
 
-    for (var objects of json) {
+    for (const objects of json) {
+      // If the user ID doesn't exist in the dictionary, add it
       if (tasks[objects.userId] === undefined) {
         tasks[objects.userId] = 0;
       }
 
+      // Else, check if the task was completed
       if (objects.completed) {
         tasks[objects.userId] += 1;
       }
