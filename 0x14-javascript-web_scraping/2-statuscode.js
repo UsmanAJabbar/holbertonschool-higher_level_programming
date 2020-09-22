c10bea45aa79280a2b2c669fbda9bc968c5419d5
UsmanGTA <usmangta@gmail.com>
2020-09-22 17:39:07 +0000

    added initial commit for task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Returns the status code after pinging a GET
+// request at a specific URL.
+const req = require('request');
+const endpoint = process.argv[2];
+
+req(endpoint, function (err, res, body) {
+  res.statusCode = !(err) ? console.log(res.statusCode) : {};
+});
