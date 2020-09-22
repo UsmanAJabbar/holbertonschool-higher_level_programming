9d0d1988d08ee513597c3574518b7220c3a23e31
UsmanGTA <usmangta@gmail.com>
2020-09-22 16:58:25 +0000

    added semistandard fixes for task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,10 @@
 // Reads content from the terminal and writes it to
 // a specified file
 
-const filename = process.argv[2]
-const content = process.argv[3]
-const fs = require('fs')
+const filename = process.argv[2];
+const content = process.argv[3];
+const fs = require('fs');
 
 fs.writeFile(filename, content, 'utf8', function (err, data) {
-  data = (err) ? console.log(err) : {}
-})
+  data = (err) ? console.log(err) : {};
+});
